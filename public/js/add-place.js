40db02325f07fea3a4ff359e622f1aea8c0e9695
jwhityoung <jwhityoung@gmail.com>
2020-07-29 14:21:07 -0500

    frontendjs

--- public/js/add-place.js
+++ public/js/add-place.js
@@ -1,39 +1,33 @@
-$(document).ready(function () {
-    //Getting references 
-    var addPlaceForm = $("form.add-place-form");
-    var userNameInput = $("#add-place-user-name");
-    var placeNameInput = $("#add-place-name");
-    var placeDescriptionInput = $("#add-place-description");
+$(document).ready(function() {
+  //Getting references 
+  var userNameInput = $("#add-place-user-name");
+  var placeNameInput = $("#add-place-name");
+  var placeDescriptionInput = $("#add-place-description");
 
 
-    // When the form is submitted, we validate there's a user name, place name and description entered
-    $("#add-place-submit").on("click", function (event) {
-        event.preventDefault();
-        var userData = {
-            userName: userNameInput.val().trim(),
-            placeName: placeNameInput.val().trim(),
-            placeDescription: placeDescriptionInput.val().trim()
-        };
-        console.log(userData);
-        if (!userData.userName || !userData.placeName || !userData.placeDescription) {
+  // When the form is submitted, we validate there's a user name, place name and description entered
+  $("#add-place-submit").on("click", function(event) {
+    event.preventDefault();
+    var userData = {
+      userName: userNameInput.val().trim(),
+      placeName: placeNameInput.val().trim(),
+      placeDescription: placeDescriptionInput.val().trim()
+    };
+    console.log(userData);
+    if (
+      !userData.userName ||
+      !userData.placeName ||
+      !userData.placeDescription)
             return;
         }
 
 
-        // If we have an email and password we run the addPlace function and clear the form
+        // If we have an email and password we run the addPlace function and clear the form **NOT WORKING PROPERLY
         addPlace(userData.userName, userData.placeName, userData.placeDescription);
         userNameInput.val("");
         placeNameInput.val("");
         placeDescriptionInput.val("");
     });
+    //Still need to post into to Database 
 
-    // function addPlace(userName, placeName, placeDescription) {
-    //     $.post("/api/add-place", {
-
-    //         name: placeName, 
-    //         description: placeDescription
-            
-
-    //     })
-    // }
 });

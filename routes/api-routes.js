0b93bc8b30a9cadda9602c0ecdd8e7dabce8c8af
John Kim <johnk9000@gmail.com>
2020-07-30 09:43:58 -0500

    Added in routes for adding a place

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -4,28 +4,52 @@ var passport = require("../config/passport");
 
 module.exports = function(app) {
 // PLACE & MAP DATA ROUTES =============================================
-app.get("/api/authors")
-
-// Post data that is input by the user -> code ajax call for html
-app.post("/api/add-place", function(req, res) {
-  db.Place.create({
-    name: req.body.name,
-    description: req.body.description,
-    coordinates: req.body.coordinates
-  })
-  .then(() => {
-    res.redirect(307, "/api/view-place");
-  })
-  .catch(function(err) {
-    res.status(401).json(err);
+  // POST data that is input by the user -> code ajax call for html
+  app.post("/api/add-place", function(req, res) {
+    db.Place.create({
+      name: req.body.placeName,
+      user: req.body.userName,
+      description: req.body.placeDescription,
+      coordinates: req.body.coordinates
+    })
+      .then(function() {
+        res.redirect(307, "/api/view-place");
+      })
+      .catch(function(err) {
+        res.status(401).json(err);
+      });
   });
-});
+  // GET data for all places to be rendered in a list (dropdown or otherwise)
+  app.get("/api/place", function(req, res) {
+    db.Place.findAll({}).then(function(dbPlace) {
+      res.json(dbPlace);
+    });
+  });
 
-app.get()
+  // GET data for place by id -> on landing page, user searches for a place or clicks link with associated place
+  app.get("/api/place/:id", function(req, res) {
+    db.Place.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPlace) {
+      res.json(dbPlace);
+    });
+  });
 
-// REVIEW & ASSOCIATED PLACE ROUTES ====================================
+  app.delete("/api/place/:id", function(req, res) {
+    db.Place.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPlace) {
+      res.json(dbPlace);
+    });
+  });
 
-// USER LOGIN & SIGNUP ROUTES ==========================================  
+  // REVIEW & ASSOCIATED PLACE ROUTES ====================================
+
+  // USER LOGIN & SIGNUP ROUTES ==========================================  
   app.post("/api/login", passport.authenticate("local"), function(req, res) {
     res.json(req.user);
   });

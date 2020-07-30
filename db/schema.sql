f22b8dd6394e7f7bd5a6673b36bf2de8ba8f3cd2
John Kim <johnk9000@gmail.com>
2020-07-30 09:37:01 -0500

    Made changes to schema sql so that tables get created

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,15 @@
 DROP DATABASE IF EXISTS Project2Dev;
 CREATE DATABASE Project2Dev;
 
+USE Project2Dev;
+
 CREATE TABLE `place` (
     `id` INT AUTO_INCREMENT NOT NULL,
   `name` VARCHAR( 255 ) NOT NULL,
   `description` VARCHAR( 255) NOT NULL,
-  `cordinates` Int( 11 ) AUTO_INCREMENT NOT NULL,
+  `coordinates` GEOMETRY NOT NULL,
 
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `review` (
@@ -16,13 +18,13 @@ CREATE TABLE `review` (
   `body` VARCHAR( 255) NOT NULL,
   `rating` Int( 11 ) NOT NULL,
 
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `user` (
     `id` INT AUTO_INCREMENT NOT NULL,
   `email` VARCHAR( 255 ) NOT NULL,
-  `password` VARCHAR( 255) NOT NULL
+  `password` VARCHAR( 255) NOT NULL,
 
-  PRIMARY KEY ('id')
+  PRIMARY KEY (`id`)
 );

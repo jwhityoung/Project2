7483219e271dd399b71a203e26b8dbd05602a265
John Kim <johnk9000@gmail.com>
2020-07-30 12:53:23 -0500

    'prettier' fixes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -3,7 +3,7 @@ var db = require("../models");
 //var passport = require("../config/passport");
 
 module.exports = function(app) {
-// PLACE & MAP DATA ROUTES =============================================
+  // PLACE & MAP DATA ROUTES =============================================
   // POST data that is input by the user -> code ajax call for html
   app.post("/api/add-place", function(req, res) {
     console.log("API Routes: " + JSON.stringify(req.body));
@@ -58,37 +58,37 @@ module.exports = function(app) {
 
   // REVIEW & ASSOCIATED PLACE ROUTES ====================================
 
-  // USER LOGIN & SIGNUP ROUTES ==========================================  
-//   app.post("/api/login", passport.authenticate("local"), function(req, res) {
-//     res.json(req.user);
-//   });
+  // USER LOGIN & SIGNUP ROUTES ==========================================
+  //   app.post("/api/login", passport.authenticate("local"), function(req, res) {
+  //     res.json(req.user);
+  //   });
 
-//   app.post("/api/signup", function(req, res) {
-//     db.User.create({
-//       email: req.body.email,
-//       password: req.body.password
-//     })
-//       .then(function() {
-//         res.redirect(307, "/api/login");
-//       })
-//       .catch(function(err) {
-//         res.status(401).json(err);
-//       });
-//   });
+  //   app.post("/api/signup", function(req, res) {
+  //     db.User.create({
+  //       email: req.body.email,
+  //       password: req.body.password
+  //     })
+  //       .then(function() {
+  //         res.redirect(307, "/api/login");
+  //       })
+  //       .catch(function(err) {
+  //         res.status(401).json(err);
+  //       });
+  //   });
 
-//   app.get("/logout", function(req, res) {
-//     req.logout();
-//     res.redirect("/");
-//   });
+  //   app.get("/logout", function(req, res) {
+  //     req.logout();
+  //     res.redirect("/");
+  //   });
 
-//   app.get("/api/user_data", function(req, res) {
-//     if (!req.user) { // Use if we want User login
-//       res.json({});
-//     } else {
-//       res.json({
-//         email: req.user.email,
-//         id: req.user.id
-//       });
-//     }
-//   });
+  //   app.get("/api/user_data", function(req, res) {
+  //     if (!req.user) { // Use if we want User login
+  //       res.json({});
+  //     } else {
+  //       res.json({
+  //         email: req.user.email,
+  //         id: req.user.id
+  //       });
+  //     }
+  //   });
 };
